40c27dce9d62ff8712144ba098601ec7fa6eaf94
sikahita <salsasikahita@gmail.com>
2022-08-21 20:35:20 +0700

    update dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -19,9 +19,7 @@ function Dashboard() {
         <div className="container" style={{ marginTop: "100px"}}>
         <h3>Hi, {user?.email}!</h3>
             <p>
-              Right now I'm busy working at a Payment Gateway company in Jakarta while studying at Binus Online Learning majoring in S1 Information Systems. 
-              It's been almost 4 years I migrated to Jakarta, whoaa time flies! And I'm studying already in 6th semesters, yay! According to the schedule, 
-              I will graduate in the middle of 2023. I hope I can graduate on time~ Aamiin
+              Welcome to Admin Dashboard System. <br/>This website was created by Salsa Dian Sikahita - 2301928036
             </p> 
             <br/>
             <button className="btn btn-primary rounded p-3 px-5" onClick={handleSignOut}>Sign out</button>
